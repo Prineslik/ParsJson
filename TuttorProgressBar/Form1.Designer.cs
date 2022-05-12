71f3e6a09e6868639fcea726861fc72c77e15aef
Prineslik <prineslikilua@gmail.com>
2022-05-12 16:29:49 +0300

    Parse
    search for three random users

---
+++ TuttorProgressBar/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿
+namespace TuttorPars
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(319, 388);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(145, 50);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Play";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(839, 370);
+            this.panel1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(863, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ TuttorProgressBar/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TuttorPars
+{
+    public partial class Form1 : Form
+    {
+        class JsonUser
+        {
+            public int userId = 0;
+            public int Id = 0;
+            public string title = "null";
+            public string body = "null";
+
+            public JsonUser(int UserId_, int Id_, string Title_, string Body_)
+            {
+                userId = UserId_;
+                Id = Id_;
+                title = Title_;
+                body = Body_;
+            }
+
+            public string Print()
+            {
+                return $"UserID - {userId};\nId - {Id};\nTitle - {title};\nBody - {body}\n\n";
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            label1.Text = "";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string jtext;
+            const string userIdPt = @"""userId""\:\s\d+";
+            const string idPt = @"""id""\:\s\d+";
+            const string titlePt = @"""title""\:\s""\D+"",";
+            const string bodyPt = @"""body""\:\s""\D+""\n";
+
+            Regex regex = new Regex(userIdPt);
+            Regex regex2 = new Regex(idPt);
+            Regex regex3 = new Regex(titlePt);
+            Regex regex4 = new Regex(bodyPt);
+
+            Random random = new Random();
+
+            JsonUser[] jsonUsers = new JsonUser[3];
+            using (WebClient wc = new WebClient()) 
+            {
+                byte[] myDataBuffer = wc.DownloadData(@"https://jsonplaceholder.typicode.com/posts");
+                jtext = Encoding.ASCII.GetString(myDataBuffer);
+
+                MatchCollection userIdC = regex.Matches(jtext);
+                MatchCollection idC = regex2.Matches(jtext);
+                MatchCollection titleC = regex3.Matches(jtext);
+                MatchCollection bodyC = regex4.Matches(jtext);
+
+                for (int i = 0; i < 3; i++)
+                {
+                    int r = random.Next(0, 100);
+                    int val1 = Convert.ToInt32(userIdC[r].Value.Remove(0, 10));
+                    int val2 = Convert.ToInt32(idC[r].Value.Remove(0, 5));
+                    string val3 = titleC[r].Value.Remove(0, 9);
+                    string val4 = bodyC[r].Value.Remove(0, 8);
+                    jsonUsers[i] = new JsonUser(val1, val2, val3, val4);
+                }
+                foreach (var item in jsonUsers)
+                {
+                    label1.Text += item.Print();
+                }
+            }
+        }
+    }
+}
